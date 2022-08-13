67d945f3af335f1c87f9ce8afefba72a0a88b981
SofiFerrarotti <106496505+SofiFerrarotti@users.noreply.github.com>
2022-08-13 20:50:57 -0300

    Create Ejerciciofinal.py

---
+++ Ejerciciofinal.py
@@ -0,0 +1,9 @@
+perro = ["13", "puppy","12/12/2012","macho","123"]
+
+for x in range (len(perro)):
+    if perro[x]=="12/12/2012" :
+        perro [x]="13/12/2012"
+for x in range (len (perro)):
+    if perro[x]== "123":
+        perro[x]= "28957346" 
+print (perro)
